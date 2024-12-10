92c75d5bc08bd3a76f046bf3578a846ce6ff3929
Johnny Console <johnny.console@hotmail.com>
2024-12-10 08:41:54 -0500

    Update Dependency Versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.johnnconsole.android.ims"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.johnnconsole.android.ims"
