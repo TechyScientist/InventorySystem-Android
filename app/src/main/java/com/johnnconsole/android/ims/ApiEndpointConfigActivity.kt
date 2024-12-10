bc506da2d9d6e05bd5c62fbca2481b7252d289e6
Johnny Console <johnny.console@hotmail.com>
2024-12-09 20:56:48 -0500

    Get and save API Endpoint and Sign In Script locations

--- app/src/main/java/com/johnnconsole/android/ims/ApiEndpointConfigActivity.kt
+++ app/src/main/java/com/johnnconsole/android/ims/ApiEndpointConfigActivity.kt
@@ -2,6 +2,8 @@ package com.johnnconsole.android.ims
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_LONG
+import com.google.android.material.snackbar.Snackbar
 import com.johnnconsole.android.ims.databinding.ActivityApiEndpointConfigBinding
 
 class ApiEndpointConfigActivity : AppCompatActivity() {
@@ -12,5 +14,25 @@ class ApiEndpointConfigActivity : AppCompatActivity() {
         binding = ActivityApiEndpointConfigBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar!!.title = "Configure API Endpoint"
+        var prefs = getSharedPreferences("AndroidIMS", MODE_PRIVATE)
+
+        with(binding) {
+            etEndpoint.setText(prefs.getString("API_ENDPOINT", ""))
+            etSignInScript.setText(prefs.getString("SIGN_IN_SCRIPT", ""))
+
+            btSaveAPI.setOnClickListener {_ ->
+                if(!etEndpoint.text.isNullOrBlank() && !etSignInScript.text.isNullOrBlank()) {
+                    val editor = prefs.edit()
+                    editor.putString("API_ENDPOINT", etEndpoint.text.toString())
+                    editor.putString("SIGN_IN_SCRIPT", etSignInScript.text.toString())
+                    editor.apply()
+                    finish()
+                }
+                else {
+                    Snackbar.make(root, "Missing API Information", LENGTH_LONG).show()
+                }
+            }
+
+        }
     }
 }
