0a5ce00fd93de1d7f73da251b029f7276cbc38e8
Johnny Console <johnny.console@hotmail.com>
2024-12-10 08:13:37 -0500

    Refactor fragment classes for organization

--- app/src/main/java/com/johnnconsole/android/ims/fragment/UserFunctionsFragment.kt
+++ app/src/main/java/com/johnnconsole/android/ims/fragment/UserFunctionsFragment.kt
@@ -1,10 +1,11 @@
-package com.johnnconsole.android.ims
+package com.johnnconsole.android.ims.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.johnnconsole.android.ims.R
 
 class UserFunctionsFragment : Fragment() {
 
