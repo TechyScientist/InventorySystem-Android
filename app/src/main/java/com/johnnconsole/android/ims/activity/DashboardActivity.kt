45c2c0865f2b9598b5cf46f2c3dc21355b9712f4
Johnny Console <johnny.console@hotmail.com>
2024-12-11 20:26:41 -0500

    Remove TabBar from dashboard

--- app/src/main/java/com/johnnconsole/android/ims/activity/DashboardActivity.kt
+++ app/src/main/java/com/johnnconsole/android/ims/activity/DashboardActivity.kt
@@ -19,32 +19,14 @@ class DashboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        supportActionBar!!.title = "Dashboard"
+        supportActionBar!!.title = "${if(ApplicationSession.access == 1) "Admin " else ""}Dashboard"
 
         with(binding) {
             tvWelcome.text = getString(R.string.DashboardWelcome, ApplicationSession.first_name)
-            val userTab = tlFunctions.newTab().setText("User Functions")
-            val adminTab = tlFunctions.newTab().setText("Admin Functions")
-            tlFunctions.addTab(userTab)
-            if(ApplicationSession.access == 1) tlFunctions.addTab(adminTab)
-            supportFragmentManager.beginTransaction().add(fvTabContent.id, UserFunctionsFragment()).commitNow()
-            tlFunctions.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
-                override fun onTabSelected(tab: TabLayout.Tab?) {
-                    supportFragmentManager.beginTransaction().replace(fvTabContent.id,
-                        if(tlFunctions.selectedTabPosition == 0) UserFunctionsFragment()
-                        else AdminFunctionsFragment()
-                    ).commitNow()
-                }
-
-                override fun onTabUnselected(tab: TabLayout.Tab?) {
-                    // Intentionally Blank
-                }
-
-                override fun onTabReselected(tab: TabLayout.Tab?) {
-                    // Intentionally Blank
-                }
-
-            })
+            supportFragmentManager.beginTransaction().add(fvTabContent.id,
+                if(ApplicationSession.access == 1) AdminFunctionsFragment()
+                else UserFunctionsFragment()
+            ).commitNow()
         }
     }
 
