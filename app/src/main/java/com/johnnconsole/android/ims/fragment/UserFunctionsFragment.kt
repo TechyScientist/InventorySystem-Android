702bbb65932f51ceb9867b935ae4d049c8f49746
Johnny Console <johnny.console@hotmail.com>
2024-12-11 20:51:48 -0500

    Add code framework for creating UserFunctionsFragment view with view binding

--- app/src/main/java/com/johnnconsole/android/ims/fragment/UserFunctionsFragment.kt
+++ app/src/main/java/com/johnnconsole/android/ims/fragment/UserFunctionsFragment.kt
@@ -5,16 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.johnnconsole.android.ims.R
+import androidx.fragment.app.FragmentActivity
+import com.johnnconsole.android.ims.databinding.FragmentUserFunctionsBinding
 
 class UserFunctionsFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_functions, container, false)
+    private lateinit var binding: FragmentUserFunctionsBinding
+    private lateinit var activity: FragmentActivity
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentUserFunctionsBinding.inflate(inflater)
+        activity = requireActivity()
+
+        return binding.root
     }
 
 }
