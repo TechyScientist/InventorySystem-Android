31b12030f4da5fee3f130e6681dc6c5ef3f0dbee
Johnny Console <johnny.console@hotmail.com>
2024-12-11 20:48:40 -0500

    Add functionality to change API settings in Admin Fragment

--- app/src/main/java/com/johnnconsole/android/ims/fragment/AdminFunctionsFragment.kt
+++ app/src/main/java/com/johnnconsole/android/ims/fragment/AdminFunctionsFragment.kt
@@ -1,20 +1,29 @@
 package com.johnnconsole.android.ims.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.johnnconsole.android.ims.R
+import androidx.fragment.app.FragmentActivity
+import com.johnnconsole.android.ims.activity.ApiEndpointConfigActivity
+import com.johnnconsole.android.ims.databinding.FragmentAdminFunctionsBinding
 
 class AdminFunctionsFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_functions, container, false)
+    private lateinit var binding: FragmentAdminFunctionsBinding
+    private lateinit var activity: FragmentActivity
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentAdminFunctionsBinding.inflate(layoutInflater)
+        activity = requireActivity()
+        with(binding) {
+            btConfigureAPI.setOnClickListener {_ ->
+                activity.startActivity(Intent(activity, ApiEndpointConfigActivity::class.java))
+            }
+        }
+        return binding.root
     }
 
 }
